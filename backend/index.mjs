36d51e4a004ec18e83d7e276e7a53747e5901150
Faithy4444 <faithy4444@gmail.com>
2025-09-10 02:07:26 +0200

    api created

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,2 +1,36 @@
 import express from "express";
-import cors from cors;
+import cors from "cors";
+
+const app =express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
+const messages = [
+    {
+        name: "Faith",
+        message: "Hi how are you?"
+        
+    },
+     {
+        name: "Fortunate",
+        message: "I am okay"
+        
+    }
+    
+]
+
+app.get("/",(req, res)=>{
+    res.json(messages);
+});
+
+app.post("/",(req, res)=>{
+const newMessage = req.body;
+messages.push(newMessage);
+res.status(201).json(newMessage);
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
